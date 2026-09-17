d5e8f570a6a18749cbfbcc44925d3cec9e0ca473
Marcos André <m.andre0215@gmail.com>
2026-09-17 12:23:40 +0000

    download_data function

---
+++ functions/download_data.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import yfinance as yf
+
+def download_data(
+    tickers:str,
+    multi_level_index:bool = False
+) -> pd.DataFrame:
+
+    """
+    Downloads the data from yahoo finance.
+    Args:
+        tickers(str): The ticker.
+        multi_level_index(bool): Remode/Include row indexes.
+
+    
+    """
+
+    result = yf.download(
+        tickers = 'AAPL',
+        multi_level_index = False
+        ).reset_index()
+
+    return result
+
